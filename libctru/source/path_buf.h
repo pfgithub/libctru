13e35d7f19c51c334bf575fcf80b653edc0a0abe
pfg <pfg@pfg.pw>
2024-05-28 15:45:22 -0500

    HACK: make thread local path buffers global

--- libctru/source/path_buf.h
+++ libctru/source/path_buf.h
@@ -2,5 +2,11 @@
 #include <stdint.h>
 #include <limits.h>
 
+// <>HACK!
+// This will cause race conditions when using path related functions
+// on two threads at the same time!
+#define __thread
+// </>
+
 extern char     __thread __ctru_dev_path_buf[PATH_MAX+1];
 extern uint16_t __thread __ctru_dev_utf16_buf[PATH_MAX+1];
